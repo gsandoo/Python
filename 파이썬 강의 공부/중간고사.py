988162e62cb6fb2244039f0c219da03651e78b45
gsandoo <rogersddt@naver.com>
2022-02-05 19:59:19 +0900

    python

--- 파이썬 강의 공부/중간고사.py
+++ 파이썬 강의 공부/중간고사.py
@@ -1,81 +1,92 @@
-# sum=0
-# n=int(input("숫자(정수)를 입력해주세요:"))
-# for i in range(1,n):
-#     if i%2==0:
-#         sum=sum+i
-# print(sum)
+# # sum=0
+# # n=int(input("숫자(정수)를 입력해주세요:"))
+# # for i in range(1,n):
+# #     if i%2==0:
+# #         sum=sum+i
+# # print(sum)
 
 
-# n=input("문자를 압력하시오.")
-# palindrome=("",)
-# if n == palindrome:
-#     print(n)
+# # n=input("문자를 압력하시오.")
+# # palindrome=("",)
+# # if n == palindrome:
+# #     print(n)
 
-# menu={"americano":2000,"cafelatte":2500,"greentea":3000,"mocha":3500}
-# n=input("메뉴를 골라주세요:")
-# print(menu['americano'])
+# # menu={"americano":2000,"cafelatte":2500,"greentea":3000,"mocha":3500}
+# # n=input("메뉴를 골라주세요:")
+# # print(menu['americano'])
 
-# for i in range(2,10):
-#     print(i,"단")
-#     for k in range(2,10):
-#         prod=i*k
-#         print(i,"*",k,"=",prod)
-
-
-
-# line= '='*20
-# case='case 1'
-# print(line)
-# print("       case")
-# print(line)
-
-
-# l="="*20
-# for i in range(1,1000):
-#         print(l)
-#         print("    Test Case",i)
-#         print(l)
-#         nn=input("NN입력:")
-#         n=int(nn[1])
-#         nn=int(nn)
-#         i+=1
-#         for k in range(0,n+1,1):
-#             print(k,n,"+",n,k,"=",nn)
-#             k=k+1
-#             n=n-1
-#         else:
-#             continue
+# # for i in range(2,10):
+# #     print(i,"단")
+# #     for k in range(2,10):
+# #         prod=i*k
+# #         print(i,"*",k,"=",prod)
 
 
 
+# # line= '='*20
+# # case='case 1'
+# # print(line)
+# # print("       case")
+# # print(line)
 
-line = '=' * 20
-TestCase = 1
 
-while(True):
-    print(line)
-    print('     Test Case', TestCase)
-    print(line)
+# # l="="*20
+# # for i in range(1,1000):
+# #         print(l)
+# #         print("    Test Case",i)
+# #         print(l)
+# #         nn=input("NN입력:")
+# #         n=int(nn[1])
+# #         nn=int(nn)
+# #         i+=1
+# #         for k in range(0,n+1,1):
+# #             print(k,n,"+",n,k,"=",nn)
+# #             k=k+1
+# #             n=n-1
+# #         else:
+# #             continue
 
-    N = input('NN 입력 : ')
-    M = N[0]
 
-    result = 0
 
-    num = int(M)
 
-    for i in range(0, int(M) + 1, 1):
-        if num == -1:
-            break
+# line = '=' * 20
+# TestCase = 1
 
-        for j in range(0, int(M) + 1, 1):
-            result = (i * 10 + j) + (j * 10 + i)
+# while(True):
+#     print(line)
+#     print('     Test Case', TestCase)
+#     print(line)
 
-            if(result == int(N)):
-                if(i != j):
-                    print(i, num, '+', num, i, '=', N)
-                    num -= 1
-                elif(i == j):
-                    num -= 1
+#     N = input('NN 입력 : ')
+#     M = N[0]
 
-    TestCase += 1
+#     result = 0
+
+#     num = int(M)
+
+#     for i in range(0, int(M) + 1, 1):
+#         if num == -1:
+#             break
+
+#         for j in range(0, int(M) + 1, 1):
+#             result = (i * 10 + j) + (j * 10 + i)
+
+#             if(result == int(N)):
+#                 if(i != j):
+#                     print(i, num, '+', num, i, '=', N)
+#                     num -= 1
+#                 elif(i == j):
+#                     num -= 1
+
+#     TestCase += 1
+
+
+
+
+  
+
+    
+
+
+
+
